a46ce85baf6f471682da5397fda9ec0344c8a149
Christian Polanco <chrispolanco@sbcglobal.net>
2019-07-22 04:59:19 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,6 +2,10 @@ class Transfer
   attr_reader :sender, :receiver, :transfer_amount
   
   def initialize(sender, receiver, transfer_amount)
+    @sender = sender 
+    @receiver = receiver
+    @transer_amount = transer_amount
+    @status = "pending"
     
   end 
 end
